4c42b84bef7ace2a16de14e81ebfd86cba3aa8a6
Jarrod Bataille <jtbataille@gmail.com>
2020-09-16 17:06:24 -0400

    modifying .sql files

--- schema.sql
+++ schema.sql
@@ -1,26 +1,26 @@
 -- Drops the employeeTracker if it already exists --
 DROP DATABASE IF EXISTS employeeTracker;
--- Create a database called employeeTracker --
+-- Creates a database called employeeTracker --
 CREATE DATABASE employeeTracker;
 USE employeeTracker;
 
--- Create a table within employeeTracker called 'department' --
+-- Creates a table within employeeTracker called 'department' --
 CREATE TABLE department (
-	id INT,
-    name VARCHAR(30),
+	id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
--- Create a table within employeeTracker called 'role' --
+-- Creates a table within employeeTracker called 'role' --
 CREATE TABLE role (
 	id INT AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8, 2) NOT NULL,
     department_id INT,
     PRIMARY KEY (id)
 );
 
--- Create a table within employeeTracker called 'employee' --
+-- Creates a table within employeeTracker called 'employee' --
 CREATE TABLE employee (
 	id INT AUTO_INCREMENT,
     first_name VARCHAR(30),
--- seed.sql
+++ seed.sql
@@ -1,26 +1,40 @@
-INSERT INTO department (id, name)
+-- Creates the following rows in the 'department' table --
+INSERT INTO department (name)
 VALUES
-(1, "Sales"),
-(2, "Engineering"),
-(3, "Legal"),
-(4, "Finance");
+("Sales"),
+("Engineering"),
+("Legal"),
+("Finance");
 
+-- Creates the following rows in the 'role' table --
 INSERT INTO role (title, salary, department_id)
 VALUES
-("Sales Lead", "100000", 1),
-("Salesperson", "80000", 1),
-("Lead Engineer", "150000", 2),
-("Software Engineer", "120000", 2),
-("Account Manager", "130000", 1),
-("Accountant", "125000", 4),
-("Lawyer", "190000", 3),
-("Legal Team Lead", "250000", 3);
+("Sales Lead Manager", "100000.00", 1),
+("Salesperson", "80000.00", 1),
+("Lead Engineer Manager", "150000.00", 2),
+("Software Engineer", "120000.00", 2),
+("Legal Team Lead Manager", "250000.00", 3),
+("Lawyer", "190000.00", 3),
+("Account Manager", "130000.00", 4),
+("Accountant", "125000.00", 4);
 
+-- Creates the following rows in the 'employee' table --
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 ("John", "Doe", 1, NULL),
-("Mike", "Chan", 4, NULL),
-("Ashley", "Rodriguez", 3, 2),
-("Kevin", "Tupik", 2, 2),
-("Tom", "Allen", 7, 1),
-("Malia", "Brown", 8, 1);
+("Mike", "Chan", 2, 1),
+("Frank", "Adams", 2, 1),
+("Ashley", "Rodriguez", 3, NULL),
+("Kevin", "Tupik", 4, 3),
+("Mason", "Ochoa", 4, 3),
+("Tom", "Allen", 5, NULL),
+("Malia", "Brown", 6, 5),
+("Xiang", "Reily", 6, 5),
+("Sally", "Ford", 7, NULL),
+("Danny", "Fitz", 8, 7),
+("Lauren", "Folley", 8, 7);
+
+-- Selects all columns from each table in database --
+SELECT * FROM employee;
+SELECT * FROM role;
+SELECT * FROM department;
